1e6f3b38b623fbae9584a2d79c0aea515ba2e73c
Prath <prathme47@gmail.com>
2024-07-09 18:11:44 +0530

    height changes

--- src/pages/SimpleEditor.jsx
+++ src/pages/SimpleEditor.jsx
@@ -10,12 +10,11 @@ const SimpleEditor = () => {
 return (
     <div className='w-full h-full'>
         <Navbar />
-        <div className='min-w-[56rem] mx-auto my-0 py-[2.3rem] px-[3rem] h-[90%] flex justify-center'>
+        <div className='min-w-[56rem] mx-auto my-0 py-[2.3rem] px-[3rem] flex justify-center'>
             <MarkdownEditor
                 // enableEmoji={true}
                 source={value}
                 value={value}
-                height="200px"
                 onChange={(e) => setValue(e)}
                 className="w-full"
                 defaultValue="//some comment"
